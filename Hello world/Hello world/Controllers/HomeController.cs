ce265cc1154d04030c09a6aec8b9ce0ff13dd0a3
HelenJose <helenchittooparmbil@gmail.com>
2017-08-02 16:56:05 +0930

    Sports management

---
+++ Hello world/Hello world/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hello_world.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About Sports Management.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "PRT 455 Group B.";
+
+            return View();
+        }
+    }
+}
